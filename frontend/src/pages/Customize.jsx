be88f11108202e7e906eb6e111ae2a9ea31edae7
Sagardussa <92858280+Sagardussa@users.noreply.github.com>
2025-09-15 12:24:50 +0530

    Update Customize.jsx

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { userDataContext } from "../context/UserContext";
-import Card from "../components/card";
+import Card from "../components/Card";
 import image1 from "../assets/image1.png";
 import image2 from "../assets/image2.jpg";
 import image3 from "../assets/authBg.png";
